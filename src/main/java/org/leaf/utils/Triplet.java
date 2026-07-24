af72517c974761ad006c59220ee8128ff5c459d0
technospirit <d.boryskin21@assomption.bzh>
2026-07-25 01:00:28 +0200

    Added a Triplet Class.

---
+++ src/main/java/org/leaf/utils/Triplet.java
@@ -0,0 +1,13 @@
+package org.leaf.utils;
+
+public class Triplet<U, V, W> {
+    public U first;
+    public V second;
+    public W third;
+
+    public Triplet(U first, V second, W third) {
+        this.first = first;
+        this.second = second;
+        this.third = third;
+    }
+}
